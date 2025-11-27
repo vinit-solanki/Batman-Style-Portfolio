8784923949d53a24e692caef59ade3c9cd2c3db3
Vinit Solanki <2022.vinit.solanki@ves.ac.in>
2025-11-27 17:24:49 +0530

    updated the font

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   theme: {
-    extend: {
-      fontFamily: {
-        science: ['"Science Gothic"', "sans-serif"],
-        raleway: ['"Raleway"', "sans-serif"],
-      },
+  extend: {
+    fontFamily: {
+      batmanA: ['"BatmanFontA"', "sans-serif"],
+      batmanO: ['"BatmanFontO"', "sans-serif"],
     },
   },
+}
 })
